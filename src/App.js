0e1f0d9f9eff919af63fddbaf529c7c8d5512f51
Debora Durek <debora.durek@gmail.com>
2021-04-13 17:55:39 -0500

    feat: Update component composition

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import * as BooksAPI from './BooksAPI'
 import './App.css';
+import Bookshelf from './Bookshelf';
+import SeachBook from './SeachBook';
+import { Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class BooksApp extends Component {
+  render() {
+    return (
+      <div className="app">
+        <Route exact path="/" render={() => <Bookshelf />} />
+        <Route path="/search" render={() => <SeachBook />} />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default BooksApp;
