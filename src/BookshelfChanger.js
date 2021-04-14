820c81e82ecbceb2e535d14a75b907645d55ae35
Debora Durek <debora.durek@gmail.com>
2021-04-14 14:14:07 -0500

    feat: Add function to update selected shelf

--- src/BookshelfChanger.js
+++ src/BookshelfChanger.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-const BookshelfChanger = () => (
-  <div className="book-shelf-changer">
-    <select>
-      <option value="move" disabled>
-        Move to...
-      </option>
-      <option value="currentlyReading">Currently Reading</option>
-      <option value="wantToRead">Want to Read</option>
-      <option value="read">Read</option>
-      <option value="none">None</option>
-    </select>
-  </div>
-);
+class BookshelfChanger extends Component {
+  static propTypes = {
+    onUpdateShelf: PropTypes.func.isRequired,
+  };
+
+  updateShelf = (event) => {
+    const chosenShelf = event.target.value;
+    this.props.onUpdateShelf(chosenShelf);
+  };
+
+  render() {
+    return (
+      <div className="book-shelf-changer">
+        <select
+          name="bookshelf-selector"
+          defaultValue="currentlyReading"
+          onChange={this.updateShelf}
+        >
+          <option value="move" disabled>
+            Move to...
+          </option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+        </select>
+      </div>
+    );
+  }
+}
 
 export default BookshelfChanger;
