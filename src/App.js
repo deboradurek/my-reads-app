f83e0e3d88c9afa06b343514a4221f0c8d864ada
Debora Durek <debora.durek@gmail.com>
2021-04-15 15:52:06 -0500

    feat: Implement initial fetch calls

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
 import React, { Component } from 'react';
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI';
 import './App.css';
-import Bookshelf from './Bookshelf';
-import SeachBook from './SeachBook';
+import BookshelvesPage from './BookshelvesPage';
+import SearchBook from './SearchBook';
 import { Route } from 'react-router-dom';
 
 class BooksApp extends Component {
+  state = {
+    books: [],
+  };
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({
+        books,
+      });
+    });
+  }
+
+  refreshBookshelves = () => {
+    BooksAPI.getAll().then((books) => {
+      this.setState({
+        books,
+      });
+    });
+  };
+
   render() {
     return (
       <div className="app">
-        <Route exact path="/" render={() => <Bookshelf />} />
-        <Route path="/search" render={() => <SeachBook />} />
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <BookshelvesPage books={books} onRefreshBookshelves={this.refreshBookshelves} />
+          )}
+        />
+        <Route
+          path="/search"
+          render={() => (
+            <SearchBook
+              booksByShelf={booksByShelf}
+              onRefreshBookshelves={this.refreshBookshelves}
+            />
+          )}
+        />
       </div>
     );
   }
