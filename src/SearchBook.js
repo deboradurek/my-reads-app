fa86b81fd9dd05304f0522ad2563ebac0deb9218
Debora Durek <debora.durek@gmail.com>
2021-04-15 11:27:09 -0500

    feat: Update file name, add state and props

--- src/SearchBook.js
+++ src/SearchBook.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 // import PropTypes from 'prop-types';
+import * as BooksAPI from './BooksAPI';
+import Bookshelf from './Bookshelf';
 
 class SeachBook extends Component {
   //   static propTypes = {};
 
+  state = {
+    query: '',
+    booksFound: [],
+  };
+
   render() {
+    const { query, booksFound } = this.state;
+    const { onRefreshBookshelves } = this.props;
+    console.log('booksfound', booksFound);
+
     return (
       <div className="search-books">
         <div className="search-books-bar">
           <Link to="/">
-            <button
-              className="close-search"
-              onClick={() => this.setState({ showSearchPage: false })}
-            >
-              Close
-            </button>
+            <button className="close-search">Close</button>
           </Link>
-          <div className="search-books-input-wrapper">
+          <form className="search-books-input-wrapper" onChange={this.handleQuery}>
             {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
                   You can find these search terms here:
@@ -26,14 +32,12 @@ class SeachBook extends Component {
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
             <input type="text" placeholder="Search by title or author" />
-          </div>
+          </form>
         </div>
-        <div className="search-books-results">
-          <ol className="books-grid"></ol>
-        </div>
+        <div className="search-books-results"></div>
       </div>
     );
   }
 }
 
-export default SeachBook;
+export default SearchBook;
