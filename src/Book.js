c8bda27ec07b23d6a6a5a9daaa188ed66a1194fd
Debora Durek <debora.durek@gmail.com>
2021-04-15 11:45:09 -0500

    feat: Add fetch to handle shelf updates

--- src/Book.js
+++ src/Book.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
-// import * as BooksAPI from './BooksAPI';
+import PropTypes from 'prop-types';
+import BookshelfChanger from './BookshelfChanger';
+import * as BooksAPI from './BooksAPI';
 
 class Book extends Component {
+  static propTypes = {
+    book: PropTypes.object.isRequired,
+    onRefreshBookshelves: PropTypes.func.isRequired,
+  };
+
+  handleUpdateShelf = (newShelf) => {
+    BooksAPI.update(this.props.book, newShelf).then(() => {
+      this.props.onRefreshBookshelves();
+    });
+  };
+
   render() {
     const { book } = this.props;
+    // console.log(book);
 
     return (
       <div className="book">
@@ -17,6 +30,7 @@ class Book extends Component {
               backgroundImage: `url("${book.imageLinks.thumbnail}")`,
             }}
           ></div>
+          <BookshelfChanger onUpdateShelf={this.handleUpdateShelf} shelf={book.shelf} />
         </div>
         <div className="book-title">{book.title}</div>
         <div className="book-authors">{book.authors.join(', ')}</div>
