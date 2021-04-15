8b3b171d23cc5b0cd91d329591be91120f019d4c
Debora Durek <debora.durek@gmail.com>
2021-04-15 11:48:41 -0500

    feat: Create component to hold each bookshelf

---
+++ src/Bookshelf.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Book from './Book';
+
+class Bookshelf extends Component {
+  static propTypes = {
+    booksPerShelf: PropTypes.array.isRequired,
+    onRefreshBookshelves: PropTypes.func.isRequired,
+  };
+
+  render() {
+    const { booksPerShelf, onRefreshBookshelves } = this.props;
+    return (
+      <ol className="books-grid">
+        {booksPerShelf.map((book) => (
+          <li key={book.id}>
+            <Book book={book} onRefreshBookshelves={onRefreshBookshelves} />
+          </li>
+        ))}
+      </ol>
+    );
+  }
+}
+
+export default Bookshelf;
