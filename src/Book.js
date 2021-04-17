0dc782357ca0d687c190968f86bf4c5890590d7d
Debora Durek <debora.durek@gmail.com>
2021-04-16 21:21:36 -0500

    feat: Change to class and add option to favourite

--- src/Book.js
+++ src/Book.js
@@ -1,38 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import BookshelfChanger from './BookshelfChanger';
-import * as BooksAPI from './BooksAPI';
-import coverNotFound from './images/default_book_cover.jpg';
+import CoverNotFound from './images/default_book_cover.jpg';
+import StarBorder from './icons/star-border.svg';
+import StarSolid from './icons/star-solid.svg';
 
-const Book = ({ book, onRefreshBookshelves }) => {
-  const handleUpdateShelf = (newShelf) => {
-    BooksAPI.update(book, newShelf).then(() => {
-      onRefreshBookshelves();
-    });
+class Book extends Component {
+  static propTypes = {
+    book: PropTypes.object.isRequired,
+    onUpdateBook: PropTypes.func.isRequired,
   };
 
-  return (
-    <div className="book">
-      <div className="book-top">
-        <div
-          className="book-cover"
-          style={{
-            width: 128,
-            height: 192,
-            backgroundImage: `url("${book.imageLinks?.thumbnail ?? coverNotFound}")`,
-          }}
-        ></div>
-        <BookshelfChanger onUpdateShelf={handleUpdateShelf} shelf={book.shelf} />
-      </div>
-      <div className="book-title">{book.title}</div>
-      <div className="book-authors">{book.authors?.join(', ')}</div>
-    </div>
-  );
-};
+  state = {
+    favourite: localStorage.getItem(this.props.book.id) === 'true',
+  };
 
-Book.propTypes = {
-  book: PropTypes.object.isRequired,
-  onRefreshBookshelves: PropTypes.func.isRequired,
-};
+  render() {
+    const { book } = this.props;
+    const starImage = this.state.favourite ? StarSolid : StarBorder;
+
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div
+            className="favourite-book"
+            style={{ backgroundImage: `url("${starImage}")` }}
+            onClick={this.setFavourite}
+          ></div>
+          <div
+            className="book-cover"
+            style={{
+              width: 128,
+              height: 192,
+              backgroundImage: `url("${book.imageLinks?.thumbnail ?? CoverNotFound}")`,
+            }}
+          ></div>
+          <BookshelfChanger shelf={book.shelf} />
+        </div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">{book.authors?.join(', ')}</div>
+      </div>
+    );
+  }
+}
 
 export default Book;
