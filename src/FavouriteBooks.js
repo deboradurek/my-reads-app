60f9270f46d8ca8d93fd330a76eb917821c7ca36
Debora Durek <debora.durek@gmail.com>
2021-04-19 17:28:41 -0500

    refactor: Transform class to function

--- src/FavouriteBooks.js
+++ src/FavouriteBooks.js
@@ -1,24 +1,27 @@
-import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import BookshelfResults from './BookshelfResults';
 
-class FavouriteBooks extends Component {
-  static propTypes = {};
-
-  render() {
-    return (
-      <div>
-        <div className="back-to-main">
-          <Link to="/">
-            <button className="close-search">Close</button>
-          </Link>
-          <span>Back To Bookshelves</span>
-        </div>
-        {/* <BookshelfResults />; */}
+const FavouriteBooks = ({ books, onUpdateBook }) => {
+  return (
+    <div>
+      <div className="back-to-main">
+        <Link to="/">
+          <button className="close-search">Close</button>
+        </Link>
+        <span>Back To Bookshelves</span>
       </div>
-    );
-  }
-}
+      <div className="search-books-results">
+        <BookshelfResults onUpdateBook={onUpdateBook} />
+      </div>
+    </div>
+  );
+};
+
+FavouriteBooks.prototypes = {
+  books: PropTypes.array.isRequired,
+  onUpdateBook: PropTypes.func.isRequired,
+};
 
 export default FavouriteBooks;
